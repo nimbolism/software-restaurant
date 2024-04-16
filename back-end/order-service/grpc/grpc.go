23de5d978b4f886fcfdf2d74f47f521cc9616c8d
nimbolism <nima_heidari@mathdep.iust.ac.ir>
2024-04-16 23:56:32 +0330

    finished order service & voucher service

--- back-end/order-service/grpc/grpc.go
+++ back-end/order-service/grpc/grpc.go
@@ -1,17 +1,23 @@
 package grpc
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
 	card_proto "github.com/nimbolism/software-restaurant/back-end/card-service/proto"
+	"github.com/nimbolism/software-restaurant/back-end/database/models"
 	food_proto "github.com/nimbolism/software-restaurant/back-end/food-service/proto"
+	"github.com/nimbolism/software-restaurant/back-end/gutils/postgresapp"
 	"github.com/nimbolism/software-restaurant/back-end/order-service/proto"
 	user_proto "github.com/nimbolism/software-restaurant/back-end/user-service/proto"
 	voucher_proto "github.com/nimbolism/software-restaurant/back-end/voucher-service/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 var (
@@ -30,6 +36,93 @@ var (
 
 type Server struct {
 	proto.UnimplementedOrderServiceServer
+}
+
+func (s *Server) GetAllOrders(ctx context.Context, req *proto.GetAllOrdersRequest) (*proto.GetAllOrdersResponse, error) {
+	db := postgresapp.DB
+	var orders []models.Order
+	if err := db.Find(&orders).Error; err != nil {
+		return nil, status.Error(
+			codes.NotFound, fmt.Sprintf("could not find orders: %v", err),
+		)
+	}
+	return &proto.GetAllOrdersResponse{Orders: convertToProtoOrders(orders)}, nil
+}
+
+func (s *Server) GetAllOrdersByUsername(ctx context.Context, req *proto.GetAllOrdersByUsernameRequest) (*proto.GetAllOrdersResponse, error) {
+	err := InitializeUserGRPCClient()
+	if err != nil {
+		return nil, status.Error(
+			codes.NotFound, fmt.Sprintf("could not initialize connection: %v", err),
+		)
+	}
+	userIDResponse, err := UserServiceClient.GetOneUser(context.Background(), &user_proto.GetOneUserRequest{Username: req.Username})
+	if err != nil {
+		return nil, status.Error(
+			codes.NotFound, fmt.Sprintf("could not get response from user service: %v", err),
+		)
+	}
+	db := postgresapp.DB
+	var orders []models.Order
+	if err := db.Where("user_id = ?", userIDResponse.UserId).Find(&orders).Error; err != nil {
+		return nil, status.Error(
+			codes.NotFound, fmt.Sprintf("could not get orders from database: %v", err),
+		)
+	}
+	return &proto.GetAllOrdersResponse{Orders: convertToProtoOrders(orders)}, nil
+}
+
+func (s *Server) GetAllOrdersBetweenTimestamps(ctx context.Context, req *proto.GetAllOrdersBetweenTimestampsRequest) (*proto.GetAllOrdersResponse, error) {
+	db := postgresapp.DB
+	var orders []models.Order
+	if err := db.Where("created_at BETWEEN ? AND ?", req.StartTime.AsTime(), req.EndTime.AsTime()).Find(&orders).Error; err != nil {
+		return nil, status.Error(
+			codes.NotFound, fmt.Sprintf("could not find orders: %v", err),
+		)
+	}
+	return &proto.GetAllOrdersResponse{Orders: convertToProtoOrders(orders)}, nil
+}
+
+func convertToProtoOrders(orders []models.Order) []*proto.Order {
+	protoOrders := make([]*proto.Order, len(orders))
+	for i, order := range orders {
+		protoOrder := &proto.Order{
+			Id:     uint64(order.ID),
+			UserId: uint64(order.UserID),
+			Paid:   order.Paid,
+			CreatedAt: &timestamppb.Timestamp{
+				Seconds: order.CreatedAt.Unix(),
+				Nanos:   int32(order.CreatedAt.Nanosecond()),
+			},
+			UpdatedAt: &timestamppb.Timestamp{
+				Seconds: order.UpdatedAt.Unix(),
+				Nanos:   int32(order.UpdatedAt.Nanosecond()),
+			},
+		}
+
+		protoFoods := make([]*proto.OrderFood, len(order.Foods))
+		for j, food := range order.Foods {
+			protoFoods[j] = &proto.OrderFood{
+				Id:          int64(food.ID),
+				Name:        food.Name,
+				Description: food.Description,
+			}
+		}
+		protoOrder.Foods = protoFoods
+
+		protoSideDishes := make([]*proto.OrderSideDish, len(order.SideDishes))
+		for k, sideDish := range order.SideDishes {
+			protoSideDishes[k] = &proto.OrderSideDish{
+				Id:          int64(sideDish.ID),
+				Name:        sideDish.Name,
+				Description: sideDish.Description,
+			}
+		}
+		protoOrder.SideDishes = protoSideDishes
+
+		protoOrders[i] = protoOrder
+	}
+	return protoOrders
 }
 
 func StartServer() {
